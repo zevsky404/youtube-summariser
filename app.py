a5ef15b7f1357cc6c9743d6b014ce9e2cfcd2531
zevsky404 <chrissiguetter2001@gmail.com>
2022-07-17 00:21:13 +0200

    finalise get_transcript function and catch more errors

--- app.py
+++ app.py
@@ -6,19 +6,34 @@ import json
 
 app = Flask(__name__)
 
+"""
+takes a video id for a YouTube video, fetches all available transcripts available for it and finds the English one
+or translates the first available transcript into English, if possible. It then parses the returned object and returns 
+the text of the transcript.
+@:param video-id: video ID of YouTube video
+@:return: transcript of the YouTube video
+"""
 
-def get_transcript(video_id, language=None):
-    if language is None:
-        language = ['en']
 
-    script = YouTubeTranscriptApi.get_transcript(video_id, language)
+def get_transcript(video_id):
+    script_list = YouTubeTranscriptApi.list_transcripts(video_id)
+    try:
+        script = script_list.find_transcript(['en'])
+        final_script = script.fetch()
+    except:
+        script = script_list[0]
+        if script.is_translatable:
+            translated_script = script.translate('en')
+            final_script = translated_script.fetch()
+        else:
+            print("Script for this video is not available and also not translatable into English.")
     formatter = JSONFormatter()
-    json_formatted = formatter.format_transcript(script)
+    json_formatted = formatter.format_transcript(final_script)
     json_object = json.loads(json_formatted)
     text = ''
     for entry in json_object:
         text += entry['text'] + ' '
-    return text 
+    return text
 
 
 
@@ -28,7 +43,7 @@ def index():
     return "Hello world!"
 
 def main():
-    get_transcript('j6PbonHsqW0')
+    print(get_transcript('tRBeGm0QMvU'))
 
 
 if __name__ == '__main__':
